ede1d93495a6036cf2d9b09963a071dbc90e4191
OmBRz <omarbrrcl@gmail.com>
2024-06-05 07:28:50 -0500

    chore: implemented base configuration

--- GlideGo-Backend.API/Program.cs
+++ GlideGo-Backend.API/Program.cs
@@ -1,11 +1,55 @@
+using GlideGo_Backend.API.Shared.Infrastructure.Interfaces.ASP.Configuration;
+using GlideGo_Backend.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//Add Configuration for Routing
+builder.Services.AddControllers(options => options.Conventions.Add(new KebabCaseRouteNamingConvention()));
+
+//Configure Lowercase URLs
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+//Add Database Connection
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Configure Database Context and Logging Levels
+
+ builder.Services.AddDbContext<AppDbContext>(options =>
+ {
+     if (connectionString == null) return;
+     if (builder.Environment.IsDevelopment()) 
+         options.UseMySQL(connectionString)
+             .LogTo(Console.WriteLine, LogLevel.Information)
+             .EnableSensitiveDataLogging()
+             .EnableDetailedErrors();
+     else if (builder.Environment.IsProduction()) 
+         options.UseMySQL(connectionString)
+             .LogTo(Console.WriteLine, LogLevel.Error)
+             .EnableDetailedErrors();
+ });
+ 
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title   = "Glidego.API",
+            Version = "v1",
+            Description = "GLIDEGO API",
+            TermsOfService = new Uri("https://glidego.com/tos"),
+            Contact = new OpenApiContact{ Name = "Glidego", Email = "contact@glidego.com" },
+            License = new OpenApiLicense { Name = "Apache 2.0", Url = new Uri("https://www.apache.org/licenses/LICENSE-2.0.html")},
+        });
+    });
 
 var app = builder.Build();
 
---
+++ GlideGo-Backend.API/Shared/Infrastructure/Persistence/EFC/Repositories/UnitOfWork.cs
@@ -0,0 +1,9 @@
+using GlideGo_Backend.API.Shared.Domain.Repositories;
+using GlideGo_Backend.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+namespace GlideGo_Backend.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+public class UnitOfWork(AppDbContext context): IUnitOfWork
+{
+    public async Task CompleteAsync() => await context.SaveChangesAsync();
+}
