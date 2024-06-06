1a7cfe3522b34b7e2af8653858e596935739f711
QuiqueVladimir <U202022365@upc.edu.pe>
2024-06-05 20:34:19 -0500

    feat(shared): added logic from persistence

--- GlideGo-Backend.API/Shared/Infrastructure/Persistence/EFC/Repositories/UnitOfWork.cs
+++ GlideGo-Backend.API/Shared/Infrastructure/Persistence/EFC/Repositories/UnitOfWork.cs
@@ -1,6 +1,18 @@
+using GlideGo_Backend.API.Shared.Domain.Repositories;
+using GlideGo_Backend.API.Shared.Infrastructure.Persistence.EFC.Configuration;
 namespace GlideGo_Backend.API.Shared.Infrastructure.Persistence.EFC.Repositories;
 
-public class UnitOfWork
+public class UnitOfWork : IUnitOfWork
 {
-    
+    private readonly AppDbContext _context;
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CompleteAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
 }
