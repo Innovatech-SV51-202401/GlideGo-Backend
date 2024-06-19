5a379b22804ff60f286daf0799502362ff93d329
sebastian <u201611430@upc.edu.pe>
2024-06-18 20:42:07 -0500

    feat(iam): added hashing, persistence, pipeline and tokens.

--- GlideGo-Backend.API/Shared/Infrastructure/Persistence/EFC/Repositories/BaseRepository.cs
+++ GlideGo-Backend.API/Shared/Infrastructure/Persistence/EFC/Repositories/BaseRepository.cs
@@ -4,14 +4,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GlideGo_Backend.API.Shared.Infrastructure.Persistence.EFC.Repositories;
 
-public abstract class BaseRepository<TEntity, TKey>(AppDbContext context) : IBaseRepository<TEntity, TKey>
+public abstract class BaseRepository<TEntity>(AppDbContext context) : IBaseRepository<TEntity>
     where TEntity : class
 {
     protected readonly AppDbContext Context = context;
 
     public async Task AddAsync(TEntity entity) => await Context.Set<TEntity>().AddAsync(entity);
 
-    public async Task<TEntity?> FindByIdAsync(TKey id) => await Context.Set<TEntity>().FindAsync(id);
+    public async Task<TEntity?> FindByIdAsync(int id) => await Context.Set<TEntity>().FindAsync(id);
 
     public void Update(TEntity entity) => Context.Set<TEntity>().Update(entity);
 
