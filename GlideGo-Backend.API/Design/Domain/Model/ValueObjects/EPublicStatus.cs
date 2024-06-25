963dc3fefc2bfca6df3d613958916228f8fd342c
QuiqueVladimir <U202022365@upc.edu.pe>
2024-06-25 13:42:30 -0500

    feat(design): added value objects for publication

---
+++ GlideGo-Backend.API/Design/Domain/Model/ValueObjects/EPublicStatus.cs
@@ -0,0 +1,9 @@
+namespace GlideGo_Backend.API.Design.Domain.Model.ValueObjects;
+
+public enum EPublicStatus
+{
+    Available,
+    Unavailable,
+    InUse,
+    InReview
+}
---
+++ GlideGo-Backend.API/Design/Domain/Model/ValueObjects/EPublicType.cs
@@ -0,0 +1,7 @@
+namespace GlideGo_Backend.API.Design.Domain.Model.ValueObjects;
+
+public enum EPublicType
+{
+    Electric,
+    Manual
+}
